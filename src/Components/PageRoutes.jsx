c864965089b78c9b846f52779444940c00289a7e
Rasul <54539257+Rasulms@users.noreply.github.com>
2024-03-11 18:15:03 +0530

    program component

--- src/Components/PageRoutes.jsx
+++ src/Components/PageRoutes.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './Home'
+import React from "react";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./Home";
+import Yoga from "./Yoga";
 
 const PageRoutes = () => {
-    return (
-        <BrowserRouter >
-            <Routes>
-                <Route path='/' element= {<Home />} />
-            </Routes>
-        </BrowserRouter>
-    )
-}
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/yoga" element={<Yoga />} />
+			</Routes>
+		</BrowserRouter>
+	);
+};
 
-export default PageRoutes
+export default PageRoutes;
